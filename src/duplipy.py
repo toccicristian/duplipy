1db9b5aacaeb0f263db8b4ca322961f1f680dca0
Cristian Tocci <cristian@cristian-g480.cristian.net>
2025-10-16 18:35:35 -0300

    actualizado a v0.1a

--- src/duplipy.py
+++ src/duplipy.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 from os.path import normpath, expanduser, isdir, isfile
 import os
 import sys
@@ -9,7 +11,7 @@ import copy
 
 # A partir de cuantos bytes deja de comparar contenido
 # y comienza a comprarar tamaños:
-DETALLE=8192 # DEPENDIENDO DEL SISTEMA, VALORES MENORES A 1024, 2048, 4096 O 8192 (o 65536 para MS EXCHANGE) NO VALGAN LA PENA.
+DETALLE=20480
 LOGFILE="duplilog.txt"
 
 ############      FIN DE VALORES EDITABLES    ################
@@ -18,9 +20,49 @@ LOGFILE="duplilog.txt"
 EXTENSIONES_PERMITIDAS=False
 
 
+buffer=""
+archivos_totales=0
+buffer_archivos_totales=f"(totales 0)"
+
 def loguea(cadena):
     with open(os.path.normpath(os.path.expanduser(LOGFILE)),"a") as arlog:
         arlog.writelines(cadena+"\n")
+
+
+def borra_linea(cadena):
+    print("",end='\r',flush=True)
+    for _ in range(0,len(cadena)):
+        print(" ",end='',flush=True)
+    print("",end='\r',flush=True)
+    return None
+
+
+def reemplaza_texto(linea_vieja, linea_nueva):
+    for x in range (0, len(linea_vieja)):
+        print("",end='\b',flush=True)
+
+    print(linea_nueva,end='',flush=True)
+    if (len(linea_nueva)<len(linea_vieja)):
+        for x in range (0, len(linea_vieja)-len(linea_nueva)):
+            print(" ",end='',flush=True)
+
+        for x in range (0, len(linea_vieja)-len(linea_nueva)):
+            print("",end='\b',flush=True)
+
+    return None
+
+
+def reemplaza_linea(linea_vieja, linea_nueva):
+    print("",end="\r",flush=True)
+    print(linea_nueva,end='',flush=True)
+    if (len(linea_nueva)<len(linea_vieja)):
+        for x in range (0, len(linea_vieja)-len(linea_nueva)):
+            print(" ",end='',flush=True)
+
+        for x in range (0, len(linea_vieja)-len(linea_nueva)):
+            print("",end='\b',flush=True)
+
+    return None
 
 
 # hace una comparacion detallada del contenido si el archivo1 es pequeño (<DETALLE)
@@ -54,10 +96,23 @@ def ldir (d, lista=list()):
     try:
         for ruta in os.listdir(d):
             if isfile(os.path.join(d,ruta)) and (not restringido(os.path.join(d,ruta))):
-                print(f"leyendo...{acorta_url(os.path.join(d,ruta),80)}")
+                salida=f"leyendo...{acorta_url(os.path.join(d,ruta),80)}"
+                global buffer
+                reemplaza_linea(buffer, salida)
+                buffer=salida
+                # print(f"leyendo...{acorta_url(os.path.join(d,ruta),80)}")
                 lista.append( (os.path.join(d,ruta),os.stat(os.path.join(d,ruta)).st_size) ) #agrego URL y Tamaño como tupla a la lista
             if isdir(os.path.join(d,ruta)):
                 ldir (os.path.join(d,ruta), lista)
+
+            # TODO : Capaz me conviene presentar esto a la izquierda siempre
+            # global archivos_totales
+            # global buffer_archivos_totales
+            # archivos_totales+=1
+            # salida=f"(totales {archivos_totales})"
+            # reemplaza_texto(buffer_archivos_totales, salida)
+            # buffer_archivos_totales=salida
+
     except (PermissionError, OSError) as e:
         print(f"\nExcepcion {e} en {d}\n")
         loguea(f"Excepcion {e} en {d}")
@@ -73,11 +128,17 @@ def busca_duplicados(d):
     cant_procesados=0
     print("Buscando duplicados...")
     for f1 in lista1:
+        # salida=f"comparando {cant_procesados}/{cant_elementos} ({(cant_procesados*100/cant_elementos):.2f}%)...{os.path.split(f1[0])[1]}"
+        salida=f"comparando {cant_procesados}/{cant_elementos} ({(cant_procesados*100/cant_elementos):.2f}%)..."
+        print(f"{salida}",end='\r',flush=True)
+        #print(f"comparando {cant_procesados}/{cant_elementos} ({(cant_procesados*100/cant_elementos):.2f}%)...{os.path.split(f1[0])[1]}")
         for f2 in lista2:
-            print(f"comparando {cant_procesados}/{cant_elementos}...{os.path.split(f1[0])[1]} == {os.path.split(f2[0])[1]}?")
+            # print(f"comparando {cant_procesados}/{cant_elementos} ({(cant_procesados*100/cant_elementos):.2f}%)...{os.path.split(f1[0])[1]} == {os.path.split(f2[0])[1]}?")
+            # print(f"comparando {cant_procesados}/{cant_elementos}...{os.path.split(f1[0])[1]} == {os.path.split(f2[0])[1]}?")
             if comparacion_selectiva(f1, f2) and str(f1[0]) != str(f2[0]):
-                print(f"(duplicados);{f1[0]};{f2[0]}")
-                loguea(f"(duplicados);{f1[0]};{f2[0]}")
+                print(f"\r(duplicados);{f1[0]};{f2[0]}")
+                loguea(f"{f1[0]};{f2[0]}")
+        borra_linea(salida)
         cant_procesados+=1
         del lista2[0]
 
